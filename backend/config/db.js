3986cab267dec6260b9b7cf2935d9fbce9025b48
1vignesh23 <mvigneshwarreddycs@gmail.com>
2025-06-15 22:50:01 +0530

    updated db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,22 +3,20 @@ require('dotenv').config(); // Loads .env variables
 
 const connectDB = async () => {
   try {
-    // Ensure that MONGO_URI exists in your .env file
     if (!process.env.MONGO_URI) {
       throw new Error('MONGO_URI is not defined in .env');
     }
 
-    // Connecting to MongoDB
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Optional: set timeout in milliseconds (5 seconds)
+      serverSelectionTimeoutMS: 5000, 
     });
 
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    process.exit(1); // Exit the process if the connection fails
+    process.exit(1); 
   }
 };
 
